f0590805c9e43399e95c888577434316b0346d35
Nekhlesh Singh Sajwan <nekhlesh@Nekhleshs-MacBook-Air.local>
2024-07-22 19:52:42 +0530

    initialized socket and ejs

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ require('dotenv').config()
 const port = process.env.port || 3000
 
 const http  = require('http')
+const path = require('path')
 const socketio  = require('socket.io')
 const server = http.createServer(app)
 const io = socketio(server)
 
-app.get('/hey', (req,res)=>{
-    res.send('HI')
+app.set('views', path.join(__dirname, 'public', 'views'));
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, "public")))
+
+io.on("connection", function (socket){
+    console.log("connected", socket)
+})
+
+app.get('/', (req,res)=>{
+    res.render('index')
 })
 
 server.listen(port, ()=>
